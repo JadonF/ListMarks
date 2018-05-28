5073e883f49b562c4db07863f0a1b6d69e21e89e
JadonF <36163291+JadonF@users.noreply.github.com>
2018-05-28 09:57:41 -0400

    Initial commit

---
+++ ListMarks/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ListMarks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstValues = new System.Windows.Forms.ListBox();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstValues
+            // 
+            this.lstValues.FormattingEnabled = true;
+            this.lstValues.Location = new System.Drawing.Point(152, 11);
+            this.lstValues.Name = "lstValues";
+            this.lstValues.Size = new System.Drawing.Size(120, 238);
+            this.lstValues.TabIndex = 0;
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(2, 11);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(132, 26);
+            this.lblPrompt.TabIndex = 1;
+            this.lblPrompt.Text = "Enter numbers between \r\n0 and 100 and click Enter.\r\n";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(5, 101);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(5, 65);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 3;
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(5, 147);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 39);
+            this.btnAverage.TabIndex = 4;
+            this.btnAverage.Text = "Calculate Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lblPrompt);
+            this.Controls.Add(this.lstValues);
+            this.Name = "Form1";
+            this.Text = "List of Marks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstValues;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAverage;
+    }
+}
+
---
+++ ListMarks/Form1.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Created by: Jadon F
+ * Created on: 25-May-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #39 - Lists
+ * This program accepts marks from 1 - 100 and puts them in a list
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ListMarks
+{
+    public partial class Form1 : Form
+    {
+        //create the list
+        List<int> listOfInts = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+   
+        private void CalculateAverage (ref List<int> listOfInts)
+        {
+            //sum
+            int sum = 0;
+            double average = 0;
+
+            //average the values
+            foreach (int inArray in listOfInts)
+            {
+                sum = sum + inArray;
+            }
+
+            //final calc
+            average = sum / listOfInts.Count;
+            //Message box
+            MessageBox.Show("The average is " + average, "Average");
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            int userMark = -1;
+            try
+            {
+                userMark = int.Parse(txtInput.Text);
+            }
+            catch(Exception parseError)
+            {
+                Console.WriteLine("An error occured: '{0}'", parseError);
+            }
+
+            if ((userMark >= 0) && (userMark <= 100))
+            {
+                lstValues.Items.Add(userMark);
+                listOfInts.Add(userMark);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.", "Error");
+            }
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            if(listOfInts.Count == 0)
+            {
+                MessageBox.Show("Please enter at least one value.", "Error");
+            }
+            else
+            {
+                CalculateAverage(ref listOfInts);
+            }
+        }
+    }
+}
